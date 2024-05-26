5812201b311ef92d9af1d09e0664741b08b3668b
Viktorija Deksne <102534473+vdeksne@users.noreply.github.com>
2024-05-26 16:01:56 +0300

    final_V1

---
+++ src/content_option.js
@@ -0,0 +1,204 @@
+const logotext = (
+<svg class="logo-color" width="157" height="38" viewBox="0 0 157 38" fill="var(--secondary-color)" color="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg">
+<path d="M18.1139 36.798L0 19.8102L18.1139 2.82239L2.28134 19.8102L18.1139 36.798Z"/>
+<path d="M39.8383 19.7601L22.867 37.9712L5.90112 19.7601L22.867 34.4012L39.8383 19.7601Z"/>
+<path d="M44.8022 19.7842L26.7918 36.7979L42.7168 19.862L24.2997 3.14993L22.8633 25.6196C22.8633 25.6196 22.8947 3.43864 22.8781 0L44.8022 19.7861V19.7842Z"/>
+<path d="M53.4968 14.5763H54.5303L57.4661 22.0292L60.4129 14.5763H61.4464L57.9319 23.3913H57.0131L53.4987 14.5763H53.4968Z"/>
+<path d="M62.9697 14.5763H63.9514V23.3913H62.9697V14.5763Z"/>
+<path d="M65.728 14.5763H66.7097V23.3913H65.728V14.5763ZM67.1275 18.9144L71.2465 14.5763H72.4556L68.3347 18.9144L72.7181 23.3913H71.509L67.1275 18.9144Z"/>
+<path d="M75.8573 15.4091H73.0361V14.5763H79.662V15.4091H76.8408V23.3913H75.8591V15.4091H75.8573Z"/>
+<path d="M82.2372 23.0063C81.5439 22.6324 80.969 22.0698 80.5789 21.3851C80.1814 20.6485 79.9744 19.8249 79.9744 18.9884C79.9744 18.1518 80.1814 17.3283 80.5789 16.5917C80.969 15.9069 81.5402 15.3443 82.2298 14.9649C82.9564 14.5836 83.7643 14.3856 84.5851 14.3856C85.4059 14.3856 86.2138 14.5855 86.9404 14.9649C87.63 15.3443 88.2012 15.9069 88.5913 16.5917C88.9888 17.3283 89.1958 18.1518 89.1958 18.9884C89.1958 19.8249 88.9888 20.6485 88.5913 21.3851C88.2012 22.0698 87.6263 22.6324 86.933 23.0063C86.2064 23.3838 85.4022 23.58 84.5833 23.58C83.7643 23.58 82.9601 23.3838 82.2335 23.0063H82.2354H82.2372ZM86.4246 22.2438C86.9737 21.9458 87.4285 21.4979 87.7354 20.9501C88.0496 20.3468 88.2123 19.6768 88.2142 18.9976C88.2142 18.3184 88.0533 17.6466 87.7427 17.0433C87.4395 16.4973 86.9903 16.0476 86.4449 15.7441C85.8848 15.435 85.2525 15.2758 84.6128 15.2832C83.9676 15.274 83.3298 15.4294 82.7604 15.7366C82.2076 16.0365 81.751 16.488 81.4441 17.0359C81.1261 17.6392 80.9597 18.311 80.9597 18.9921C80.9597 19.675 81.1224 20.3468 81.4386 20.9501C81.7436 21.4961 82.1984 21.944 82.7493 22.2438C83.3169 22.5418 83.9473 22.6972 84.5888 22.6972C85.2303 22.6972 85.8607 22.5418 86.4283 22.2438H86.4246Z"/>
+<path d="M90.71 14.5763H94.1116C94.8622 14.5226 95.6054 14.7521 96.197 15.2185C96.6278 15.6978 96.8792 16.3123 96.9069 16.9563C96.9346 17.6004 96.7368 18.2352 96.3486 18.7497C95.9493 19.2105 95.3965 19.5103 94.792 19.5954L96.8829 23.3913H95.8624L93.7973 19.6454H91.6953V23.3913H90.7137V14.5763H90.71ZM93.9841 18.8144C94.4943 18.844 94.999 18.6923 95.4076 18.3851C95.5851 18.2277 95.7219 18.0334 95.8125 17.815C95.9012 17.5967 95.94 17.3598 95.9234 17.1247C95.9493 16.8786 95.916 16.6324 95.8273 16.4011C95.7385 16.1716 95.5962 15.9643 95.4131 15.7996C94.9935 15.5146 94.4906 15.3776 93.9841 15.4091H91.6916V18.8144H93.9841Z"/>
+<path d="M98.4321 14.5763H99.4157V23.3913H98.4321V14.5763Z"/>
+<path d="M104.45 14.5763H105.369L108.883 23.3913H107.875L106.931 21.0335H102.875L101.943 23.3913H100.935L104.45 14.5763ZM106.655 20.2006L104.905 15.7126L103.167 20.2006H106.655Z"/>
+<path d="M110.227 14.5763H113.388C114.261 14.556 115.128 14.7373 115.921 15.1038C116.616 15.4332 117.197 15.9607 117.591 16.6232C117.994 17.3265 118.195 18.1279 118.177 18.9366C118.177 20.3987 117.748 21.5073 116.892 22.2587C116.036 23.0101 114.847 23.3876 113.327 23.3876H110.229V14.5745L110.227 14.5763ZM113.514 21.5128C113.839 21.5332 114.167 21.4832 114.47 21.3629C114.773 21.2426 115.047 21.0575 115.27 20.8188C115.701 20.2895 115.919 19.6195 115.882 18.9403C115.908 18.2907 115.69 17.6541 115.27 17.1562C115.06 16.9212 114.799 16.7361 114.509 16.6158C114.218 16.4955 113.904 16.44 113.588 16.4566H112.455V21.5128H113.512H113.514Z"/>
+<path d="M119.057 14.5763H125.305V16.4548H121.286V18.1075H124.222V19.8601H121.286V21.5128H125.305V23.3913H119.057V14.5763Z"/>
+<path d="M126.187 14.5763H128.417V18.2352L131.452 14.5763H134.098L130.532 18.8644L134.336 23.3894H131.691L128.415 19.4955V23.3931H126.185V14.5781L126.187 14.5763Z"/>
+<path d="M135.875 23.4227C135.407 23.3431 134.952 23.1969 134.527 22.9878V20.9835C134.976 21.2167 135.451 21.3943 135.943 21.5109C136.403 21.6257 136.875 21.6849 137.348 21.6867C137.67 21.7071 137.991 21.6423 138.28 21.498C138.378 21.4462 138.457 21.3666 138.515 21.2722C138.572 21.1778 138.598 21.0668 138.596 20.9557C138.598 20.7854 138.539 20.6226 138.426 20.4949C138.285 20.345 138.115 20.2228 137.929 20.1358C137.71 20.0266 137.37 19.8767 136.908 19.6824C136.239 19.4344 135.632 19.0421 135.131 18.535C134.797 18.1315 134.603 17.63 134.58 17.1062C134.556 16.5825 134.704 16.0661 135 15.633C135.3 15.224 135.708 14.9057 136.178 14.7132C136.729 14.4856 137.32 14.3745 137.916 14.3856C138.398 14.3801 138.879 14.4227 139.352 14.5115C139.77 14.5948 140.177 14.7299 140.561 14.915V16.8693C139.877 16.4973 139.11 16.2974 138.332 16.29C137.999 16.2752 137.668 16.3419 137.368 16.4862C137.261 16.5343 137.169 16.6139 137.106 16.7139C137.041 16.8138 137.008 16.9285 137.01 17.047C137.012 17.1691 137.047 17.2894 137.111 17.3931C137.176 17.4967 137.267 17.5818 137.376 17.6392C137.808 17.8798 138.259 18.0852 138.723 18.2574C139.213 18.4443 139.681 18.6867 140.117 18.9754C140.424 19.1827 140.674 19.464 140.842 19.7953C141.005 20.1692 141.082 20.5745 141.068 20.9816C141.084 21.3481 141.012 21.7127 140.857 22.0458C140.702 22.3789 140.471 22.6695 140.178 22.8916C139.587 23.3505 138.703 23.5782 137.527 23.5782C136.973 23.5782 136.42 23.5245 135.876 23.4209L135.875 23.4227Z"/>
+<path d="M141.948 14.5763H144.151L147.388 19.8231V14.5763H149.618V23.3913H147.424L144.174 18.1334V23.3913H141.946V14.5763H141.948Z"/>
+<path d="M150.751 14.5763H157V16.4548H152.981V18.1075H155.917V19.8601H152.981V21.5128H157V23.3913H150.751V14.5763Z"/>
+</svg>
+);
+const meta = {
+    title: "Viktorija Deksne",
+    description: "Hi, I am Viktorija, full-stack developer living in Riga, Latvia. I love coding and I code cool websites and develop mobile apps.",
+};
+
+const introdata = {
+    title: "Hello, I am Viktorija",
+    animated: {
+        first: "Innovative full-stack web developer",
+        second: "Custom web solutions specialist",
+        third: "Seamless web developer",
+    },
+    description: "I am a creative tech professional with a rich 9-year background in design and UX/UI, 5 years in front-end development, and a recent dive into back-end magic.",
+    your_img_url: "https://raw.githubusercontent.com/vdeksne/portfolio2024_images/main/about/profile_img_V4.png",
+};
+
+const dataabout = {
+    title: "About me",
+    aboutme: "My journey blends art with code, vision with execution, crafting seamless digital experiences. Driven by curiosity and the joy of building, I’m on a quest to innovate and solve complex challenges in the tech world.",
+};
+const worktimeline = [{
+        jobtitle: "Interactive Media Designer",
+        where: "ShowHeroes Group",
+        date: "2022 - Now",
+    },
+    {
+        jobtitle: "Online Marketing Specialist",
+        where: "Your Move",
+        date: "2018 - 2021",
+    },
+    {
+        jobtitle: "Project Manager",
+        where: "Art Ovation Hotel",
+        date: "2018",
+    },
+];
+
+const skills = [{
+        name: "Figma",
+        value: 90,
+    },
+    {
+        name: "Adobe Suite",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "Python",
+        value: 60,
+    },
+    {
+        name: "Django",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 80,
+    },
+    {
+        name: "PHP",
+        value: 70,
+    },
+    {
+        name: "MySQL",
+        value: 70,
+    },
+    {
+        name: "Google Web Designer",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Crafting intuitive and visually appealing interfaces for enhanced user experiences.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Building robust, scalable websites tailored to your business needs.",
+    },
+    {
+        title: "Web Development",
+        description: "Developing seamless, high-performance mobile applications for all platforms.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://raw.githubusercontent.com/vdeksne/portfolio2024_images/main/work/1.png",
+        description: "Interactive & responsive microsite. Figma, Vanilla JS, CSS3, HTML5.",
+        link: "https://platform.showheroes.com/format-demos/preview.html#midroll=true&preview=1&f=intext&source=https%3A%2F%2Fapi.create.showheroes.com%2Fcreatives%2F4588%2Fvast%3Fpreview_duration%3D20%26swipe_area%3D74%26version%3D3%26preview%3D1%26gdpr%3D%7Bgdpr%7D%26cs%3D%7Bgdpr_consent%7D%26cb%3D%7Brandom%7D%26domain%3D%7Bdomain%7D%26supply_id%3D%7Bsite_id%7D%26demand_id%3D%7Bcampaign_id%7D%26demand_id_2%3D%7Bline_item_id%7D%26device_type%3D%7Bdevice_type%7D%26utm_medium%3D%7Bcampaign_id%7D%26utm_campaign%3D%7Bcampaign_name%7D%26utm_term%3D%7Bline_item_name%7D%26utm_content%3D%7Bline_item_id%7D",
+    },
+    {
+        img: "https://raw.githubusercontent.com/vdeksne/portfolio2024_images/main/work/2.png",
+        description: "Interactive & responsive microsite. Figma, Vanilla JS, CSS3, HTML5.",
+        link: "https://platform.showheroes.com/format-demos/preview.html#midroll=true&preview=1&f=intext&source=https%3A%2F%2Fapi.create.showheroes.com%2Fcreatives%2F4604%2Fvast%3Fpreview_duration%3D20%26swipe_area%3D74%26version%3D3%26preview%3D1%26gdpr%3D%7Bgdpr%7D%26cs%3D%7Bgdpr_consent%7D%26cb%3D%7Brandom%7D%26domain%3D%7Bdomain%7D%26supply_id%3D%7Bsite_id%7D%26demand_id%3D%7Bcampaign_id%7D%26demand_id_2%3D%7Bline_item_id%7D%26device_type%3D%7Bdevice_type%7D%26utm_medium%3D%7Bcampaign_id%7D%26utm_campaign%3D%7Bcampaign_name%7D%26utm_term%3D%7Bline_item_name%7D%26utm_content%3D%7Bline_item_id%7D",
+    },
+    {
+        img: "https://raw.githubusercontent.com/vdeksne/portfolio2024_images/main/work/3.png",
+        description: "Interactive & responsive microsite. Figma, Vanilla JS, CSS3, HTML5.",
+        link: "https://platform.showheroes.com/format-demos/preview.html#midroll=true&preview=1&f=intext&source=https%3A%2F%2Fapi.create.showheroes.com%2Fcreatives%2F3998%2Fvast%3Fpreview_duration%3D20%26swipe_area%3D74%26version%3D3%26preview%3D1%26gdpr%3D%7Bgdpr%7D%26cs%3D%7Bgdpr_consent%7D%26cb%3D%7Brandom%7D%26domain%3D%7Bdomain%7D%26supply_id%3D%7Bsite_id%7D%26demand_id%3D%7Bcampaign_id%7D%26demand_id_2%3D%7Bline_item_id%7D%26device_type%3D%7Bdevice_type%7D%26utm_medium%3D%7Bcampaign_id%7D%26utm_campaign%3D%7Bcampaign_name%7D%26utm_term%3D%7Bline_item_name%7D%26utm_content%3D%7Bline_item_id%7D",
+    },
+    {
+        img: "https://raw.githubusercontent.com/vdeksne/portfolio2024_images/main/work/4.png",
+        description: "Interactive & responsive microsite. Figma, Vanilla JS, CSS3, HTML5.",
+        link: "https://platform.showheroes.com/format-demos/preview.html#midroll=true&preview=1&f=intext&source=https%3A%2F%2Fapi.create.showheroes.com%2Fcreatives%2F3466%2Fvast%3Fpreview_duration%3D20%26swipe_area%3D74%26version%3D3%26preview%3D1%26gdpr%3D%7Bgdpr%7D%26cs%3D%7Bgdpr_consent%7D%26cb%3D%7Brandom%7D%26domain%3D%7Bdomain%7D%26supply_id%3D%7Bsite_id%7D%26demand_id%3D%7Bcampaign_id%7D%26demand_id_2%3D%7Bline_item_id%7D%26device_type%3D%7Bdevice_type%7D%26utm_medium%3D%7Bcampaign_id%7D%26utm_campaign%3D%7Bcampaign_name%7D%26utm_term%3D%7Bline_item_name%7D%26utm_content%3D%7Bline_item_id%7D",
+    },
+    {
+        img: "https://raw.githubusercontent.com/vdeksne/portfolio2024_images/main/work/5.png",
+        description: "Interactive & responsive microsite. Figma, Vanilla JS, CSS3, HTML5.",
+        link: "https://platform.showheroes.com/format-demos/preview.html#midroll=true&preview=1&f=intext&source=https%3A%2F%2Fapi.create.showheroes.com%2Fcreatives%2F3328%2Fvast%3Fpreview_duration%3D20%26swipe_area%3D74%26version%3D3%26preview%3D1%26gdpr%3D%7Bgdpr%7D%26cs%3D%7Bgdpr_consent%7D%26cb%3D%7Brandom%7D%26domain%3D%7Bdomain%7D%26supply_id%3D%7Bsite_id%7D%26demand_id%3D%7Bcampaign_id%7D%26demand_id_2%3D%7Bline_item_id%7D%26device_type%3D%7Bdevice_type%7D%26utm_medium%3D%7Bcampaign_id%7D%26utm_campaign%3D%7Bcampaign_name%7D%26utm_term%3D%7Bline_item_name%7D%26utm_content%3D%7Bline_item_id%7D",
+    },
+    {
+        img: "https://raw.githubusercontent.com/vdeksne/portfolio2024_images/main/work/6.png",
+        description: "Interactive & responsive microsite. Figma, Vanilla JS, CSS3, HTML5.",
+        link: "https://platform.showheroes.com/format-demos/preview.html#midroll=true&preview=1&f=intext&source=https%3A%2F%2Fapi.create.showheroes.com%2Fcreatives%2F3475%2Fvast%3Fpreview_duration%3D20%26swipe_area%3D74%26version%3D3%26preview%3D1%26gdpr%3D%7Bgdpr%7D%26cs%3D%7Bgdpr_consent%7D%26cb%3D%7Brandom%7D%26domain%3D%7Bdomain%7D%26supply_id%3D%7Bsite_id%7D%26demand_id%3D%7Bcampaign_id%7D%26demand_id_2%3D%7Bline_item_id%7D%26device_type%3D%7Bdevice_type%7D%26utm_medium%3D%7Bcampaign_id%7D%26utm_campaign%3D%7Bcampaign_name%7D%26utm_term%3D%7Bline_item_name%7D%26utm_content%3D%7Bline_item_id%7D",
+    },
+
+    {
+        img: "https://raw.githubusercontent.com/vdeksne/portfolio2024_images/main/work/7.png",
+        description: "Interactive & responsive microsite. Figma, Vanilla JS, CSS3, HTML5.",
+        link: "https://platform.showheroes.com/format-demos/preview.html#midroll=true&preview=1&f=intext&source=https%3A%2F%2Fapi.create.showheroes.com%2Fcreatives%2F3469%2Fvast%3Fpreview_duration%3D20%26swipe_area%3D74%26version%3D3%26preview%3D1%26gdpr%3D%7Bgdpr%7D%26cs%3D%7Bgdpr_consent%7D%26cb%3D%7Brandom%7D%26domain%3D%7Bdomain%7D%26supply_id%3D%7Bsite_id%7D%26demand_id%3D%7Bcampaign_id%7D%26demand_id_2%3D%7Bline_item_id%7D%26device_type%3D%7Bdevice_type%7D%26utm_medium%3D%7Bcampaign_id%7D%26utm_campaign%3D%7Bcampaign_name%7D%26utm_term%3D%7Bline_item_name%7D%26utm_content%3D%7Bline_item_id%7D",
+    },
+    {
+        img: "https://raw.githubusercontent.com/vdeksne/portfolio2024_images/main/work/8.png",
+        description: "Interactive & responsive microsite. Figma, Vanilla JS, CSS3, HTML5.",
+        link: "https://platform.showheroes.com/format-demos/preview.html#midroll=true&preview=1&f=intext&source=https%3A%2F%2Fapi.create.showheroes.com%2Fcreatives%2F3749%2Fvast%3Fpreview_duration%3D20%26swipe_area%3D74%26version%3D3%26preview%3D1%26gdpr%3D%7Bgdpr%7D%26cs%3D%7Bgdpr_consent%7D%26cb%3D%7Brandom%7D%26domain%3D%7Bdomain%7D%26supply_id%3D%7Bsite_id%7D%26demand_id%3D%7Bcampaign_id%7D%26demand_id_2%3D%7Bline_item_id%7D%26device_type%3D%7Bdevice_type%7D%26utm_medium%3D%7Bcampaign_id%7D%26utm_campaign%3D%7Bcampaign_name%7D%26utm_term%3D%7Bline_item_name%7D%26utm_content%3D%7Bline_item_id%7D",
+    },
+    {
+        img: "https://raw.githubusercontent.com/vdeksne/portfolio2024_images/main/work/9.png",
+        description: "Interactive & responsive microsite. Figma, Vanilla JS, CSS3, HTML5.",
+        link: "https://platform.showheroes.com/format-demos/preview.html#midroll=true&preview=1&f=intext&source=https%3A%2F%2Fapi.create.showheroes.com%2Fcreatives%2F3473%2Fvast%3Fpreview_duration%3D20%26swipe_area%3D74%26version%3D3%26preview%3D1%26gdpr%3D%7Bgdpr%7D%26cs%3D%7Bgdpr_consent%7D%26cb%3D%7Brandom%7D%26domain%3D%7Bdomain%7D%26supply_id%3D%7Bsite_id%7D%26demand_id%3D%7Bcampaign_id%7D%26demand_id_2%3D%7Bline_item_id%7D%26device_type%3D%7Bdevice_type%7D%26utm_medium%3D%7Bcampaign_id%7D%26utm_campaign%3D%7Bcampaign_name%7D%26utm_term%3D%7Bline_item_name%7D%26utm_content%3D%7Bline_item_id%7D",
+    },
+    {
+        img: "https://raw.githubusercontent.com/vdeksne/portfolio2024_images/main/work/16.png",
+        description: "Interactive & responsive microsite. Figma, Vanilla JS, CSS3, HTML5.",
+        link: "https://platform.showheroes.com/format-demos/preview.html#midroll=true&preview=1&f=intext&source=https%3A%2F%2Fapi.create.showheroes.com%2Fcreatives%2F3473%2Fvast%3Fpreview_duration%3D20%26swipe_area%3D74%26version%3D3%26preview%3D1%26gdpr%3D%7Bgdpr%7D%26cs%3D%7Bgdpr_consent%7D%26cb%3D%7Brandom%7D%26domain%3D%7Bdomain%7D%26supply_id%3D%7Bsite_id%7D%26demand_id%3D%7Bcampaign_id%7D%26demand_id_2%3D%7Bline_item_id%7D%26device_type%3D%7Bdevice_type%7D%26utm_medium%3D%7Bcampaign_id%7D%26utm_campaign%3D%7Bcampaign_name%7D%26utm_term%3D%7Bline_item_name%7D%26utm_content%3D%7Bline_item_id%7D",
+    },
+    {
+        img: "https://raw.githubusercontent.com/vdeksne/portfolio2024_images/main/work/17.png",
+        description: "Interactive & responsive microsite. Figma, Vanilla JS, CSS3, HTML5.",
+        link: "https://www.figma.com/proto/fPrbOFAJCVQxwpfZVjUsBI/Expander_1?page-id=0%3A1&node-id=13-53&viewport=-9%2C2164%2C0.41&t=rfQHkx1IDGJVYehx-1&scaling=min-zoom",
+    },
+    {
+        img: "https://raw.githubusercontent.com/vdeksne/portfolio2024_images/main/work/18.png",
+        description: "Interactive & responsive microsite. Figma, Vanilla JS, CSS3, HTML5.",
+        link: "https://www.figma.com/proto/QrYWpQaTKUbSfXUoO4Lycn/expander_2?page-id=0%3A1&node-id=18-280&viewport=-2843%2C260%2C0.45&t=4jQCUHPeoDQafNiB-1&scaling=min-zoom",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "viktorija.deksne@gmail.com",
+    YOUR_FONE: "(+371)272-66132",
+    description: "I would love to hear from you! Feel free to reach out to me through any of the following channels. Let's connect and discuss your project ideas or any opportunities for collaboration. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "gmail",
+    YOUR_TEMPLATE_ID: "template_qmw8hc3",
+    YOUR_USER_ID: "porjpAB1jVcpuBdf7",
+};
+
+const socialprofils = {
+    github: "https://github.com/vdeksne",
+    linkedin: "https://www.linkedin.com/in/vdeksne/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
